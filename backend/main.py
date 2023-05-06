6c70c8acf7d72acd59047bb2168a1542a45f24a4
shreywy <shreywy@users.noreply.github.com>
2023-05-06 12:31:19 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-
+import cohere
 
 def getSets():
     sets = ['backend\\datasets\\shakespeare.txt','backend\\datasets\\programmer.txt','backend\\datasets\\torontomans.txt', 'backend\\datasets\\plain.txt' ]
@@ -13,3 +13,6 @@ def getSets():
 if __name__ == '__main__':
     datasets = getSets()
     print(datasets)
+    
+    co = cohere.client('ihkXSqNDJRtrS3iR9HxDrWGREDDM4XR3YpbfO2gE')
+    
