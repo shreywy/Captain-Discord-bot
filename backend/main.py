968cc32a6431c13bdac3d621f819d0ce8a117a39
shreywy <shreywy@users.noreply.github.com>
2023-05-06 12:54:44 -0400

    Update to cohere file

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,32 @@
 import cohere
 
 def getSets():
-    sets = ['backend\\datasets\\shakespeare.txt','backend\\datasets\\programmer.txt','backend\\datasets\\torontomans.txt', 'backend\\datasets\\plain.txt' ]
+    sets = ['backend\\datasets\\shakespeare.txt','backend\\datasets\\programmer.txt','backend\\datasets\\torontomans.txt']
     datasets = [[],[],[]]
     for i in range(len(sets)):
         with open(sets[i],'r') as f:
             datasets[i] = f.read().split('\n')
     return datasets
 
+def getResponse(style, text):
+    
+    if style == "":
+        style = 'command-xlarge-nightly'
+    
+    co = cohere.Client('ihkXSqNDJRtrS3iR9HxDrWGREDDM4XR3YpbfO2gE') # This is your trial API key
+    response = co.generate(
+    model=style,
+    prompt=text,
+    max_tokens=300,
+    temperature=0.9,
+    k=0,
+    stop_sequences=[],
+    return_likelihoods='NONE')
+    return ('Prediction: {}'.format(response.generations[0].text))
 
 
 if __name__ == '__main__':
-    datasets = getSets()
-    print(datasets)
-    
-    co = cohere.client('ihkXSqNDJRtrS3iR9HxDrWGREDDM4XR3YpbfO2gE')
+    #datasets = getSets()
+
+    print(getResponse("", 'describe a dog'))
     
